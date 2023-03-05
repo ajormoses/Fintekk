19022cb8d3ad626e587f18ca99ca25edb173058b
ajormoses <ajormoses@gmail.com>
2023-03-05 16:31:51 +0100

    nav component iss fixed in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,31 +15,31 @@ export default {
 </script>
 
 <template>
-  <div class="fixed w-full shadow-[2px_2px_8px_0px_#2E5CFF1A]">
+  <div class="fixed w-full bg-[#fff] shadow-[2px_2px_8px_0px_#2E5CFF1A] z-30">
     <div class="container flex justify-between item-center py-5">
       <h1 class="text-[#2E5CFF] font-semibold text-[21px]">Fintekk</h1>
       <img @click="toggle" src="../assets/img/hamburger.svg" alt="hamburger" />
     </div>
-  </div>
-  <div
-    class="nav container bg-[#5BCDF0] text-center py-2 rounded-lg"
-    v-if="menu === true"
-  >
-    <ul class="leading-[40px]">
-      <li>Home</li>
-      <li>About</li>
-      <li>FAQs</li>
-      <li>Contact Us</li>
-    </ul>
-    <button
-      class="text-[#2E5CFF] border border-[#2E5CFF] bg-[#fff] mt-[15px] btn"
+    <div
+      class="nav container bg-[#5BCDF0] text-center py-2 rounded-lg z-30"
+      v-if="menu === true"
     >
-      Login
-    </button>
-    <br />
-    <button class="bg-[#2E5CFF] text-white my-[15px] btn">
-      Sign Up for Free
-    </button>
+      <ul class="leading-[40px]">
+        <li>Home</li>
+        <li>About</li>
+        <li>FAQs</li>
+        <li>Contact Us</li>
+      </ul>
+      <button
+        class="text-[#2E5CFF] border border-[#2E5CFF] bg-[#fff] mt-[15px] btn"
+      >
+        Login
+      </button>
+      <br />
+      <button class="bg-[#2E5CFF] text-white my-[15px] btn">
+        Sign Up for Free
+      </button>
+    </div>
   </div>
 </template>
 
@@ -48,7 +48,7 @@ export default {
   position: absolute;
   top: 0px;
   left: 50%;
-  transform: translate(-50%, 35%);
+  transform: translate(-50%, 30%);
   width: 100%;
 }
 
