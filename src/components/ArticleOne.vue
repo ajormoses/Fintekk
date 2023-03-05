beadfdb987c4f93500f1920493d821a0d548d54b
ajormoses <ajormoses@gmail.com>
2023-03-05 16:23:21 +0100

    articleone component completed

--- src/components/ArticleOne.vue
+++ src/components/ArticleOne.vue
@@ -18,7 +18,27 @@ export default {
         Fintekk is the bank that does it all. Manage everything directly and
         easy with fintekk.
       </p>
+      <div class="flex flex-col justify-center items-center">
+        <button
+          class="btn bg-[#2E5CFF] text-[#fff] py-[16px] px-[24px] w-[200px] mb-4"
+        >
+          Sign Up for Free
+        </button>
+        <button
+          class="btn p-[14px] w-[200px] bg-[#fff] rounded-2xl border border-[#3a3a3a] my-4 flex justify-center items-center"
+        >
+          <img class="mr-3" src="../assets/img/apple.svg" alt="apple" />
+          Get on iphone
+        </button>
+        <button
+          class="btn p-[14px] w-[200px] bg-[#fff] rounded-2xl border border-[#3a3a3a] flex justify-center items-center"
+        >
+          <img class="mr-3" src="../assets/img/andriod.svg" alt="Andriod" />
+          Get on Andriod
+        </button>
+      </div>
     </div>
+    <img src="../assets/img/img-1.svg" alt="img one" />
   </div>
 </template>
 
