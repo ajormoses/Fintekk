ff4da25f5ba53e3a46a816f751627c87e9a1276f
ajormoses <ajormoses@gmail.com>
2023-03-18 18:14:38 +0100

    Update: ArticleFive component created

---
+++ src/components/ArticleFive.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import Faqs from "./Faqs.vue";
+</script>
+
+<template>
+  <div class="bg-[#F9F9F9] section">
+    <div class="container">
+      <img src="/img/questions.svg" alt="questions" />
+      <Faqs />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
