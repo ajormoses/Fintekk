401e583afdc5f0add3d2d10ffeeae03229289331
ajormoses <ajormoses@gmail.com>
2023-03-18 19:13:01 +0100

    Update: FaqInput component created

--- src/components/Faqs.vue
+++ src/components/Faqs.vue
@@ -1,17 +1,5 @@
-<script lang="ts">
-export default {
-  data() {
-    return {
-      answer: false,
-    };
-  },
-
-  methods: {
-    open() {
-      this.answer = !this.answer;
-    },
-  },
-};
+<script lang="ts" setup>
+import FaqInput from "../components/Reusuable/FaqInput.vue";
 </script>
 
 <template>
@@ -19,24 +7,21 @@ export default {
     <div class="px-6">
       <h1 class="text-white text-[38px] font-bold">FAQs</h1>
       <div class="pt-8">
-        <div
-          @click="open"
-          class="border border-white rounded-lg flex justify-between items-center py-2 px-4"
-        >
-          <p class="text-white text-base leading-[19.4px]">
-            Is fintekk’s security guaranteed?
-          </p>
-          <div>
-            <img v-if="answer === false" src="/img/arrowdown.svg" />
-            <img v-if="answer === true" src="/img/arrowup.svg" />
-          </div>
-        </div>
-        <div v-if="answer" class="bg-white rounded-lg mt-[0.1rem]">
-          <p class="text-[#172E80] leading-[24px] px-4 pt-2 pb-8 text-[14px]">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint.
-          </p>
-        </div>
+        <FaqInput
+          question="Is fintekk’s security guaranteed?"
+          content="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint."
+        />
+        <FaqInput
+          question="How to activate Fintekk account"
+          content="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint."
+        />
+        <FaqInput
+          question="What should I do if I forget my pin?"
+          content="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint."
+        />
       </div>
     </div>
   </div>
---
+++ src/components/Reusuable/FaqInput.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+export default {
+  props: ["question", "content"],
+
+  data() {
+    return {
+      answer: false,
+    };
+  },
+
+  methods: {
+    open() {
+      this.answer = !this.answer;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="mb-4">
+    <div
+      @click="open"
+      class="border border-white rounded-lg flex justify-between items-center py-2 px-4"
+    >
+      <p class="text-white text-base leading-[19.4px]">
+        {{ question }}
+      </p>
+      <div>
+        <img v-if="answer === false" src="/img/arrowdown.svg" />
+        <img v-if="answer === true" src="/img/arrowup.svg" />
+      </div>
+    </div>
+    <div v-if="answer" class="bg-white rounded-lg mt-[0.1rem]">
+      <p class="text-[#172E80] leading-[24px] px-4 pt-2 pb-8 text-[14px]">
+        {{ content }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
