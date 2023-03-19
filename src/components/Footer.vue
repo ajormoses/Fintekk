1286faed314c5e32059714d7af00b4a6d863fb95
ajormoses <ajormoses@gmail.com>
2023-03-19 17:44:48 +0100

    Update: Footer component updated

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,40 +3,46 @@
 <template>
   <div id="contact" class="section bg-[#FBFBFB]">
     <div class="container">
-      <div class="lg:grid lg:grid-cols-5 lg:items-start lg:gap-8">
-        <div class="col-span-2">
-          <h1 class="text-[28px] leading-[33.9px] font-bold text-[#172E80]">
+      <div
+        class="lg:grid lg:grid-cols-5 xl:grid-cols-6 lg:items-start lg:gap-8"
+      >
+        <div class="lg:col-span-2">
+          <h1
+            class="text-[28px] lg:text-[38px] leading-[33.9px] font-bold text-[#172E80]"
+          >
             Fintekk
           </h1>
-          <p class="text-[#777777] text-base leading-[28px] py-3">
+          <p
+            class="text-[#777777] text-base lg:text-[20px] leading-[28px] lg:leading-[32px] py-3"
+          >
             Fintekk is the bank that does it all. Manage everything directly and
             easy with fintekk.
           </p>
         </div>
         <ul
-          class="text-[#172E80] text-base grid grid-cols-2 md:grid-cols-4 lg:col-span-3"
+          class="text-[#172E80] text-base grid grid-cols-2 md:grid-cols-4 lg:col-span-3 xl:col-span-4"
         >
-          <div class="leading-[2rem]">
+          <div class="leading-[2rem] lg:text-[20px] lg:leading-[2.5rem]">
             <h1 class="font-bold pt-4 lg:pt-0">Company</h1>
             <li>Careers</li>
             <li>About Us</li>
             <li>Privacy</li>
             <li>Terms</li>
           </div>
-          <div class="leading-[2rem]">
+          <div class="leading-[2rem] lg:text-[20px] lg:leading-[2.5rem]">
             <h1 class="font-bold pt-4 lg:pt-0">Services</h1>
             <li>Payments</li>
             <li>Investments</li>
             <li>Savings Plans</li>
           </div>
-          <div class="leading-[2rem]">
+          <div class="leading-[2rem] lg:text-[20px] lg:leading-[2.5rem]">
             <h1 class="font-bold pt-4 lg:pt-0">Resources</h1>
             <li>Media</li>
             <li>FAQs</li>
             <li>Help Centre</li>
             <li>Security</li>
           </div>
-          <div class="leading-[2rem]">
+          <div class="leading-[2rem] lg:text-[20px] lg:leading-[2.5rem]">
             <h1 class="font-bold pt-4 lg:pt-0">Contact</h1>
             <li>www.Fintekk.com</li>
           </div>
