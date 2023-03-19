2a8191a4c82bf17bb58bb8e8047c820c5d56818a
ajormoses <ajormoses@gmail.com>
2023-03-19 10:09:43 +0100

    Update: ArticleThree component updated

--- src/components/ArticleTwo.vue
+++ src/components/ArticleTwo.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div
+    id="about"
     class="h-[260px] flex justify-center items-center bg-white container text-center"
   >
     <h1 class="text-[#172E80] text-[28px] leading-[44px] font-extrabold px-8">
