817b412107080518dd2afa1604a26b446dd5dc3c
ajormoses <ajormoses@gmail.com>
2023-03-19 17:18:39 +0100

    Update: Footer updated

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,42 +3,57 @@
 <template>
   <div id="contact" class="section bg-[#FBFBFB]">
     <div class="container">
-      <h1 class="text-[28px] leading-[33.9px] font-bold text-[#172E80]">
-        Fintekk
-      </h1>
-      <p class="text-[#777777] text-base leading-[28px] py-3">
-        Fintekk is the bank that does it all. Manage everything directly and
-        easy with fintekk.
-      </p>
-      <ul class="text-[#172E80] text-base grid grid-cols-2 md:grid-cols-4">
-        <div class="leading-[2rem]">
-          <h1 class="font-bold pt-4">Company</h1>
-          <li>Careers</li>
-          <li>About Us</li>
-          <li>Privacy</li>
-          <li>Terms</li>
+      <div class="lg:grid lg:grid-cols-5 lg:items-start lg:gap-8">
+        <div class="col-span-2">
+          <h1 class="text-[28px] leading-[33.9px] font-bold text-[#172E80]">
+            Fintekk
+          </h1>
+          <p class="text-[#777777] text-base leading-[28px] py-3">
+            Fintekk is the bank that does it all. Manage everything directly and
+            easy with fintekk.
+          </p>
         </div>
-        <div class="leading-[2rem]">
-          <h1 class="font-bold pt-4">Services</h1>
-          <li>Payments</li>
-          <li>Investments</li>
-          <li>Savings Plans</li>
-        </div>
-        <div class="leading-[2rem]">
-          <h1 class="font-bold pt-4">Resources</h1>
-          <li>Media</li>
-          <li>FAQs</li>
-          <li>Help Centre</li>
-          <li>Security</li>
-        </div>
-        <div class="leading-[2rem]">
-          <h1 class="font-bold pt-4">Contact</h1>
-          <li>www.Fintekk.com</li>
-        </div>
-      </ul>
-      <p class="leading-[19.4px] text-base text-[#172E80] mt-6 font-bold">
-        Copyright 2022 fintekk. All rights reserved
-      </p>
+        <ul
+          class="text-[#172E80] text-base grid grid-cols-2 md:grid-cols-4 lg:col-span-3"
+        >
+          <div class="leading-[2rem]">
+            <h1 class="font-bold pt-4 lg:pt-0">Company</h1>
+            <li>Careers</li>
+            <li>About Us</li>
+            <li>Privacy</li>
+            <li>Terms</li>
+          </div>
+          <div class="leading-[2rem]">
+            <h1 class="font-bold pt-4 lg:pt-0">Services</h1>
+            <li>Payments</li>
+            <li>Investments</li>
+            <li>Savings Plans</li>
+          </div>
+          <div class="leading-[2rem]">
+            <h1 class="font-bold pt-4 lg:pt-0">Resources</h1>
+            <li>Media</li>
+            <li>FAQs</li>
+            <li>Help Centre</li>
+            <li>Security</li>
+          </div>
+          <div class="leading-[2rem]">
+            <h1 class="font-bold pt-4 lg:pt-0">Contact</h1>
+            <li>www.Fintekk.com</li>
+          </div>
+        </ul>
+      </div>
+      <div class="lg:flex lg:justify-between lg:items-center lg:mt-16">
+        <p
+          class="leading-[19.4px] text-base text-[#172E80] mt-6 lg:m-0 font-bold"
+        >
+          Copyright 2022 fintekk. All rights reserved
+        </p>
+        <p
+          class="hidden lg:flex leading-[19.4px] text-base text-[#172E80] font-bold"
+        >
+          Terms and Conditions, Privacy Policy
+        </p>
+      </div>
     </div>
   </div>
 </template>
