cca9985de9dcd67c23273c2b31bf736c497f25b4
ajormoses <ajormoses@gmail.com>
2023-03-19 16:54:31 +0100

    Update: updated

--- src/components/ArticleSeven.vue
+++ src/components/ArticleSeven.vue
@@ -3,21 +3,30 @@
 <template>
   <div class="section bg-white">
     <div class="container">
-      <div class="bg-[#2E5CFF] rounded-[1.5rem] px-6 py-10">
-        <h1 class="text-[38px] leading-[46.1px] font-bold text-white">
-          Start growing your wealth today
-        </h1>
-        <form @submit.prevent>
-          <input
-            class="h-[51px] bg-transparent border border-white rounded-[1rem] w-full my-5 px-5 text-white placeholder-white"
-            placeholder="Enter your email address"
-          />
-          <button
-            class="text-[#172E80] leading-[19.4px] py-[16px] px-[32px] text-base rounded-[1rem] block bg-white font-medium"
+      <div
+        class="bg-[#2E5CFF] rounded-[1.5rem] lg:rounded-[2rem] px-6 py-10 lg:py-20"
+      >
+        <div class="lg:w-[500px] lg:m-auto lg:text-center">
+          <h1
+            class="text-[38px] lg:text-[50px] lg:leading-[60px] lg:px-[3.6rem] leading-[46.1px] font-bold text-white"
           >
-            Sign up
-          </button>
-        </form>
+            Start growing your wealth today
+          </h1>
+          <form
+            @submit.prevent
+            class="lg:border lg:border-white lg:rounded-[2rem] lg:flex lg:justify-between lg:items-center lg:pl-6 lg:mt-6"
+          >
+            <input
+              class="h-[51px] lg:h-[64px] bg-transparent border border-white lg:border-transparent rounded-[1rem] lg:rounded-[2rem] w-full my-5 lg:my-0 px-5 lg:px-0 text-white placeholder-white"
+              placeholder="Enter your email address"
+            />
+            <button
+              class="text-[#172E80] lg:text-[#3A3A3A] leading-[19.4px] py-[16px] lg:py-0 px-[32px] lg:px-0 lg:h-[62px] lg:rounded-[2rem] lg:w-[145px] text-base lg:text-[21px] rounded-[1rem] block bg-white font-medium"
+            >
+              Sign up
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
--- src/components/ArticleSix.vue
+++ src/components/ArticleSix.vue
@@ -2,31 +2,46 @@
 
 <template>
   <div class="bg-[#FBFBFB] section">
-    <div class="container text-center">
-      <p class="text-base leading-[19px] text-[#3A3A3A]">TESTIMONIALS</p>
-      <h1 class="font-bold text-[#172E80] text-[38px] leading-[50px] px-4 pt-3">
-        What Our Customers Say About Us
-      </h1>
+    <div
+      class="container text-center lg:text-left lg:flex lg:justify-between lg:items-center lg:gap-8"
+    >
+      <div>
+        <p
+          class="text-base lg:text-[18px] xl:text-[24px] leading-[19px] lg:leading-[29px] text-[#3A3A3A]"
+        >
+          TESTIMONIALS
+        </p>
+        <h1
+          class="font-bold text-[#172E80] text-[38px] leading-[50px] px-4 pt-3 lg:px-0 lg:text-[40px]"
+        >
+          What Our Customers Say About Us
+        </h1>
+      </div>
       <div class="bg-white rounded-[1.5rem] p-4 mt-4">
         <p class="leading-[30px] text-base py-4 text-[#3A3A3A]">
           Dealt with enquiries and subsequent transactions efficiently and
           quickly. I’m so pleased with the pricing and service.
         </p>
-        <div class="w-[200px] m-auto">
+        <div class="w-[200px] m-auto lg:m-0">
           <h1 class="text-[#172E80] text-[24px] leading-[29px] font-medium">
             Maryam Adegoke
           </h1>
-          <img class="mt-3 ml-3" src="/img/stars.svg" />
+          <img class="mt-3 ml-3 lg:ml-0" src="/img/stars.svg" />
         </div>
       </div>
-      <div class="flex items-center justify-between w-[180px] m-auto pt-8">
-        <img src="/img/slider-left.svg" />
-        <span class="bg-[#172E80] rounded-full text-white px-1 text-[14px]"
+      <div
+        class="flex lg:flex-col items-center justify-between w-[180px] m-auto pt-8"
+      >
+        <img class="lg:mb-3 lg:hidden" src="/img/slider-left.svg" />
+        <img class="hidden lg:block lg:mb-3" src="/img/slideup.svg" />
+        <span
+          class="bg-[#172E80] rounded-full text-white px-1 text-[14px] lg:mb-3"
           >01</span
         >
-        <span class="text-[14px]">02</span>
-        <span class="text-[14px]">03</span>
-        <img src="/img/slider-right.svg" />
+        <span class="text-[14px] lg:mb-3">02</span>
+        <span class="text-[14px] lg:mb-3">03</span>
+        <img class="lg:mb-3 lg:hidden" src="/img/slider-right.svg" />
+        <img class="hidden lg:block lg:mb-3" src="/img/slidedown.svg" />
       </div>
     </div>
   </div>
