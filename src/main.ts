987a55c2a97bee359636a5a5ef35662e722e0912
ajormoses <ajormoses@gmail.com>
2023-03-19 08:34:18 +0100

    Update: Vue pagination installed

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// import the package
+import VueAwesomePaginate from "vue-awesome-paginate";
+
+// import the necessary css file
+import "vue-awesome-paginate/dist/style.css";
+
+createApp(App).use(VueAwesomePaginate).mount("#app");
