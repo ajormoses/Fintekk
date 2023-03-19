5724d202a0d2504ee739002d24dda8344637c454
ajormoses <ajormoses@gmail.com>
2023-03-19 13:35:54 +0100

    Update: ArticleTwo updated

--- src/components/ArticleTwo.vue
+++ src/components/ArticleTwo.vue
@@ -3,9 +3,11 @@
 <template>
   <div
     id="about"
-    class="h-[260px] flex justify-center items-center bg-white container text-center"
+    class="h-[260px] lg:h-[330px] flex justify-center items-center bg-white container text-center"
   >
-    <h1 class="text-[#172E80] text-[28px] leading-[44px] font-extrabold px-8">
+    <h1
+      class="text-[#172E80] text-[28px] leading-[44px] font-extrabold px-8 lg:w-[700px] lg:text-[50px] lg:leading-[72px]"
+    >
       More than <span class="text-[#2E5CFF]">10,000</span> active users trust
       Fintekk
     </h1>
