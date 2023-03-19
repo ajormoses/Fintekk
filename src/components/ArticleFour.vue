88ee3a884ab94e10e5a8e19092d7d142983915c7
ajormoses <ajormoses@gmail.com>
2023-03-19 14:29:58 +0100

    Update: Article Four/Steps components updated

--- src/components/ArticleFour.vue
+++ src/components/ArticleFour.vue
@@ -4,10 +4,15 @@ import Steps from "./Reusuable/Steps.vue";
 
 <template>
   <div class="section bg-white">
-    <div class="container">
-      <img class="mx-auto" src="/img/iphone.svg" />
+    <div class="container lg:grid lg:grid-cols-2 lg:items-center">
+      <img
+        class="mx-auto lg:m-0 lg:w-[80%] xl:w-[90%] lg:h-[760px]"
+        src="/img/iphone.svg"
+      />
       <div>
-        <h1 class="text-[28px] leading-[44px] font-bold py-16 px-6 text-center">
+        <h1
+          class="text-[28px] lg:text-[35px] xl:text-[40px] lg:px-0 text-[#172E80] leading-[44px] lg:leading-[60px] xl:leading-[72px] font-bold py-16 lg:py-10 px-6 text-center lg:text-left"
+        >
           Start the journey to reach your financial goals today.
         </h1>
         <div>
@@ -15,22 +20,29 @@ import Steps from "./Reusuable/Steps.vue";
             sn="01"
             title="Download the mobile app"
             content="Sign up for an account with your name, email and phone number."
+            bord="border border-[#2E5CFF]"
           />
           <Steps
             sn="02"
             title="Create a free account"
             content="Sign up for an account with your name, email and phone number."
+            bord="border border-[#2E5CFF]"
           />
           <Steps
             sn="03"
             title="Add a payment method"
             content="Using your debit card, bank account, QR code, perform your first transaction."
           />
-          <div class="text-center mt-16">
+          <div class="text-center mt-16 lg:mt-5">
             <button
-              class="text-white py-[16px] px-[24px] bg-[#2E5CFF] rounded-[16px]"
+              class="text-white py-[16px] px-[24px] bg-[#2E5CFF] rounded-[16px] lg:hidden"
             >
               Try Fintekk Now
+            </button>
+            <button
+              class="text-white py-[16px] px-[24px] bg-[#2E5CFF] rounded-[16px] hidden lg:inline-block"
+            >
+              Download Now
             </button>
           </div>
         </div>
--- src/components/Reusuable/Steps.vue
+++ src/components/Reusuable/Steps.vue
@@ -1,22 +1,29 @@
 <script lang="ts" setup>
-const props = defineProps(["sn", "title", "content"]);
+const props = defineProps(["sn", "title", "content", "bord"]);
 </script>
 
 <template>
-  <div class="flex relative h-[120px] mb-5">
-    <h1 class="mr-5 text-[21px] text-[#2E5CFF] font-bold leading-[25.5px]">
+  <div class="flex relative h-[120px] lg:h-[180px] mb-5 lg:mb-8">
+    <h1
+      class="mr-5 text-[21px] lg:text-[24px] text-[#2E5CFF] font-bold leading-[25.5px] lg:leading-[40px]"
+    >
       {{ sn }}
     </h1>
     <div>
-      <h1 class="leading-[25.5px] font-bold text-[21px] text-[#172E80]">
+      <h1
+        class="leading-[25.5px] font-bold text-[21px] lg:text-[24px] lg:leading-[40px] text-[#172E80]"
+      >
         {{ title }}
       </h1>
-      <p class="text-[18px] leading-[32px] text-[#777777] pt-[0.9rem] pb-5">
+      <p
+        class="text-[18px] lg:text-[24px] leading-[32px] lg:leading-[40px] text-[#777777] pt-[0.9rem] pb-5"
+      >
         {{ content }}
       </p>
     </div>
     <div
-      class="absolute left-3 bottom-0 h-[70px] border border-[#2E5CFF]"
+      class="absolute left-3 bottom-0 h-[70px] lg:h-[130px]"
+      :class="bord"
     ></div>
   </div>
 </template>
