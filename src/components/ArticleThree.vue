3cce716c068630192f12f67a7fb05c5095031b40
ajormoses <ajormoses@gmail.com>
2023-03-19 18:11:46 +0100

    Update: ArticleThree/Service updated

--- src/components/ArticleThree.vue
+++ src/components/ArticleThree.vue
@@ -7,16 +7,25 @@ import Service from "../components/Reusuable/Service.vue";
     <div class="container">
       <div class="text-center lg:w-[720px] lg:m-auto">
         <p
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-duration="1300"
           class="text-base lg:text-[24px] leading-[19.41px] lg:leading-[29px] text-[#3A3A3A]"
         >
           OUR SERVICES
         </p>
         <h1
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-duration="1500"
           class="leading-[46px] lg:leading-[60.6px] font-bold text-[38px] lg:text-[50px] text-[#172E80] pb-3"
         >
           What we offer
         </h1>
         <p
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-duration="1800"
           class="text-base lg:text-[20px] leading-[30px] lg:leading-[32px] text-[#777777]"
         >
           We help individuals become more flexible and responsible with their
--- src/components/Reusuable/Service.vue
+++ src/components/Reusuable/Service.vue
@@ -4,20 +4,41 @@ const props = defineProps(["h", "p", "url", "bg"]);
 
 <template>
   <div class="mb-8 rounded-[1.5rem] p-5 lg:p-8" :class="bg">
-    <h1 class="text-[28px] leading-[34px] text-[#172E80] font-bold">
+    <h1
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      data-aos-duration="1300"
+      class="text-[28px] leading-[34px] text-[#172E80] font-bold"
+    >
       <!-- Easy Payment -->
       {{ h }}
     </h1>
-    <p class="leading-[30px] text-base text-[#777777] py-3">
+    <p
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      data-aos-duration="1500"
+      class="leading-[30px] text-base text-[#777777] py-3"
+    >
       {{ p }}
     </p>
-    <div class="flex items-center">
+    <div
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      data-aos-duration="1800"
+      class="flex items-center"
+    >
       <p class="mr-2 text-[18px] leading-[22px] text-[#2E5CFF] font-bold">
         Read More
       </p>
       <img class="mt-[0.6px]" src="/img/arrow-right-short.svg" />
     </div>
-    <img class="mx-auto mt-5" :src="`/img/${url}`" />
+    <img
+      id="about"
+      data-aos="zoom-in"
+      data-aos-duration="2000"
+      class="mx-auto mt-5"
+      :src="`/img/${url}`"
+    />
   </div>
 </template>
 
