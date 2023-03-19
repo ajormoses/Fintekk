beb1f1bbc22c51ed711b3e8f726f656a3b874a5d
ajormoses <ajormoses@gmail.com>
2023-03-19 18:24:00 +0100

    Update: ArticleFour/Steps updated

--- src/components/ArticleFour.vue
+++ src/components/ArticleFour.vue
@@ -6,11 +6,17 @@ import Steps from "./Reusuable/Steps.vue";
   <div class="section bg-white">
     <div class="container lg:grid lg:grid-cols-2 lg:items-center">
       <img
+        data-aos="zoom-in"
+        data-aos-anchor-placement="top-bottom"
+        data-aos-duration="1300"
         class="mx-auto lg:m-0 lg:w-[80%] xl:w-[90%] lg:h-[760px]"
         src="/img/iphone.svg"
       />
       <div>
         <h1
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-duration="1500"
           class="text-[28px] lg:text-[35px] xl:text-[40px] lg:px-0 text-[#172E80] leading-[44px] lg:leading-[60px] xl:leading-[72px] font-bold py-16 lg:py-10 px-6 text-center lg:text-left"
         >
           Start the journey to reach your financial goals today.
@@ -35,11 +41,17 @@ import Steps from "./Reusuable/Steps.vue";
           />
           <div class="text-center mt-16 lg:mt-5">
             <button
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+              data-aos-duration="2100"
               class="text-white py-[16px] px-[24px] bg-[#2E5CFF] rounded-[16px] lg:hidden"
             >
               Try Fintekk Now
             </button>
             <button
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-bottom"
+              data-aos-duration="2100"
               class="text-white py-[16px] px-[24px] bg-[#2E5CFF] rounded-[16px] hidden lg:inline-block"
             >
               Download Now
--- src/components/Reusuable/Steps.vue
+++ src/components/Reusuable/Steps.vue
@@ -5,23 +5,35 @@ const props = defineProps(["sn", "title", "content", "bord"]);
 <template>
   <div class="flex relative h-[120px] lg:h-[180px] mb-5 lg:mb-8">
     <h1
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      data-aos-duration="1700"
       class="mr-5 text-[21px] lg:text-[24px] text-[#2E5CFF] font-bold leading-[25.5px] lg:leading-[40px]"
     >
       {{ sn }}
     </h1>
     <div>
       <h1
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+        data-aos-duration="1700"
         class="leading-[25.5px] font-bold text-[21px] lg:text-[24px] lg:leading-[40px] text-[#172E80]"
       >
         {{ title }}
       </h1>
       <p
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+        data-aos-duration="1900"
         class="text-[18px] lg:text-[24px] leading-[32px] lg:leading-[40px] text-[#777777] pt-[0.9rem] pb-5"
       >
         {{ content }}
       </p>
     </div>
     <div
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      data-aos-duration="2100"
       class="absolute left-3 bottom-0 h-[70px] lg:h-[130px]"
       :class="bord"
     ></div>
