2f52074fc45ad228bf45c3a2f6f357e3dfb2c61d
ajormoses <ajormoses@gmail.com>
2023-03-19 10:10:09 +0100

    Updated

--- src/components/ArticleTwo.vue
+++ src/components/ArticleTwo.vue
@@ -7,7 +7,7 @@
   >
     <h1 class="text-[#172E80] text-[28px] leading-[44px] font-extrabold px-8">
       More than <span class="text-[#2E5CFF]">10,000</span> active users trust
-      Fintekk
+      Fintekkk
     </h1>
   </div>
 </template>
